8d2bb3384860d3815caa86f82623534130efb855
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-17 17:19:28 -0400

    Simplify Android.mk radio-file logic
    * We only support bootloader.img in subdirs.

--- Android.mk
+++ Android.mk
@@ -2,11 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 define target-radio-files
 $(notdir \
-  $(wildcard $(LOCAL_PATH)/filesmap) \
-  $(wildcard $(LOCAL_PATH)/*.bin) \
-  $(wildcard $(LOCAL_PATH)/*.elf) \
-  $(wildcard $(LOCAL_PATH)/*.img) \
-  $(wildcard $(LOCAL_PATH)/*.mbn) \
+  $(wildcard $(LOCAL_PATH)/bootloader.img) \
 )
 endef
 
